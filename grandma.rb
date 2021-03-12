fb85d103c959a805dea38650a47c93922bc123fd
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-12 14:10:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,3 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  # phrase = "I LOVE YOU GRANDMA!"
-  if phrase != phrase.upcase
-    "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
-  else
-      "NO, NOT SINCE 1938!"
-  end
-end
